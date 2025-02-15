b3a616fd4b143aaff1efea1d16493e89025d6d00
xShadyy <tymoteusz.netter@gmail.com>
2025-02-15 14:48:10 +0100

    Fixed script for eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,14 @@
-import mantine from 'eslint-config-mantine';
-import tseslint from 'typescript-eslint';
+import { FlatCompat } from '@eslint/eslintrc'
 
-export default tseslint.config(...mantine, { ignores: ['**/*.{mjs,cjs,js,d.ts,d.mts}'] });
+const compat = new FlatCompat({
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.dirname,
+})
+
+const eslintConfig = [
+  ...compat.config({
+    extends: ['next'],
+  }),
+]
+
+export default eslintConfig
