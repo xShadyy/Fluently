02036bad9476cd8679c82dd8f775aaf5e8f5133d
xShadyy <tymoteusz.netter@gmail.com>
2025-02-20 14:37:05 +0100

    qf

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,6 @@
 import StarBackground from '../components/Background/StarBackground';
 import Header from '../components/Header/Header';
-import LoginForm from '../components/LoginForm/LoginForm';
+import LoginForm from '../components/LoginForm/loginForm';
 
 export default function HomePage() {
   return (
