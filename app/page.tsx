145eecd31ef08ead79616681896feca915655d03
xShadyy <tymoteusz.netter@gmail.com>
2025-02-20 13:56:05 +0100

    Made root text of the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import StarBackground from './components/Background/StarBackground';
 import Header from './components/Header/Header';
+import Root from './components/Root/Root';
 
 export default function HomePage() {
   return (
     <>
       <StarBackground />
       <Header />
+      <Root />
     </>
   );
 }
