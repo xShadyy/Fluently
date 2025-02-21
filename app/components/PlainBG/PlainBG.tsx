042bb9e9b8d2063167f31a73030a221941ec9eae
xShadyy <tymoteusz.netter@gmail.com>
2025-02-21 12:00:55 +0100

    Redo

--- app/components/PlainBG/PlainBG.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./PlainBG.module.css";
-
-const PlainBG: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
-  return <div className={styles.background}>{children}</div>;
-};
-
-export default PlainBG;
