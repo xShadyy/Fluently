6af7ece36321f53baff3393054691bffe3d63694
xShadyy <tymoteusz.netter@gmail.com>
2025-02-28 22:48:08 +0100

    Fixed tracking

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,43 @@
 "use client";
 import { useState, useEffect } from "react";
-import Spline from "@splinetool/react-spline";
-import StarBackground from "./components/ui/Background/StarBackground";
 import Header from "./components/ui/Header/Header";
 import Root from "./components/ui/RootContents/RootContents";
 import styles from "./page.module.css";
+import Spline from "@splinetool/react-spline";
 
 export default function HomePage() {
   const [startAnimation, setStartAnimation] = useState(false);
 
   useEffect(() => {
+    const script = document.createElement("script");
+    script.type = "module";
+    script.src = "https://unpkg.com/@splinetool/viewer@1.9.72/build/spline-viewer.js";
+    document.body.appendChild(script);
+
     setTimeout(() => {
       setStartAnimation(true);
     }, 2000);
+
+    return () => {
+      document.body.removeChild(script);
+    };
   }, []);
 
   return (
     <div className={styles.page}>
+
       <Header />
+
       <div className={`${styles.splineContainer} ${startAnimation ? styles.moveLeft : ""}`}>
-        <Spline scene="https://prod.spline.design/IV4HnLV5h5pOmWxH/scene.splinecode" />
+        <Spline
+          scene="https://prod.spline.design/IV4HnLV5h5pOmWxH/scene.splinecode" 
+        />
       </div>
+
       <div className={`${styles.rootContainer} ${startAnimation ? styles.showRoot : ""}`}>
         <Root />
       </div>
+      
     </div>
   );
 }
